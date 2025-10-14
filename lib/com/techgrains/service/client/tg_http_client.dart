bb454165cc44d9cbc9ef7c800000cc62b2967bfe
Ronak Upadhyay <ronak.upadhyay@techgrains.com>
2025-10-14 16:24:22 +0530

    v1.6.5: Service : TGHttpClient - For Web, used BrowserClient

--- lib/com/techgrains/service/client/tg_http_client.dart
+++ lib/com/techgrains/service/client/tg_http_client.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:http/browser_client.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
 import 'package:universal_io/io.dart';
@@ -14,22 +16,34 @@ class TGHttpClient extends http.BaseClient {
     if (_isClientInitialized) {
       return;
     }
-    HttpClient webHttpClient = new HttpClient();
-    if (badCertificateCallbackEnabled) {
-      webHttpClient.badCertificateCallback =
-          ((X509Certificate cert, String host, int port) => true);
+
+    if (kIsWeb) {
+      // On Web, use BrowserClient (wraps fetch properly)
+      _client = BrowserClient();
+    } else {
+      // On Mobile/Desktop, use IOClient
+      HttpClient webHttpClient = new HttpClient();
+      if (badCertificateCallbackEnabled) {
+        webHttpClient.badCertificateCallback =
+            ((X509Certificate cert, String host, int port) => true);
+      }
+      if (connectionTimeoutInSeconds > 0) {
+        webHttpClient.connectionTimeout =
+            Duration(seconds: connectionTimeoutInSeconds);
+      }
+      _isClientInitialized = true;
+      _client = new IOClient(webHttpClient);
     }
-    if (connectionTimeoutInSeconds > 0) {
-      webHttpClient.connectionTimeout =
-          Duration(seconds: connectionTimeoutInSeconds);
-    }
-    _isClientInitialized = true;
-    _client = new IOClient(webHttpClient);
   }
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
     request.headers.addAll(headers);
+    if (kIsWeb && connectionTimeoutInSeconds > 0) {
+      return _client
+          .send(request)
+          .timeout(Duration(seconds: connectionTimeoutInSeconds));
+    }
     return _client.send(request);
   }
 }
