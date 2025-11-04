dc33a2e8fa961a62ab49614f7d99abff3c631fe9
Ronak Upadhyay <ronak.upadhyay@techgrains.com>
2025-11-04 11:52:08 +0530

    v1.6.7: Service : TGHttpClient - For Web, used BrowserClient

--- lib/com/techgrains/service/client/tg_http_client.dart
+++ lib/com/techgrains/service/client/tg_http_client.dart
@@ -1,35 +1,40 @@
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/io_client.dart';
-import 'package:universal_io/io.dart';
+
+// Import the platform-specific client factory: uses tg_http_client_web.dart on web, tg_http_client_io.dart elsewhere
+import 'tg_http_client_io.dart' if (dart.library.html) 'tg_http_client_web.dart'
+    as platform_client;
 
 class TGHttpClient extends http.BaseClient {
   static bool badCertificateCallbackEnabled = false;
   static int connectionTimeoutInSeconds = 15;
 
   final Map<String, String> headers;
-  static http.Client _client = new IOClient();
+  static late http.Client _client;
   static bool _isClientInitialized = false;
 
   TGHttpClient({this.headers = const {}}) {
     if (_isClientInitialized) {
       return;
     }
-    HttpClient webHttpClient = new HttpClient();
-    if (badCertificateCallbackEnabled) {
-      webHttpClient.badCertificateCallback =
-          ((X509Certificate cert, String host, int port) => true);
-    }
-    if (connectionTimeoutInSeconds > 0) {
-      webHttpClient.connectionTimeout =
-          Duration(seconds: connectionTimeoutInSeconds);
-    }
+
+    // Delegate client creation to the platform-specific factory
+    _client = platform_client.createPlatformClient(
+      badCertificateCallbackEnabled: badCertificateCallbackEnabled,
+      connectionTimeoutInSeconds: connectionTimeoutInSeconds,
+    );
+
     _isClientInitialized = true;
-    _client = new IOClient(webHttpClient);
   }
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
     request.headers.addAll(headers);
+    if (kIsWeb && connectionTimeoutInSeconds > 0) {
+      return _client
+          .send(request)
+          .timeout(Duration(seconds: connectionTimeoutInSeconds));
+    }
     return _client.send(request);
   }
 }
